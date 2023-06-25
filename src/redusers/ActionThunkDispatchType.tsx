eee2eff4064b191ce0f88f4cae8a4265ed99ad32
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2023-06-25 10:45:55 +0300

    fx

---
+++ src/redusers/ActionThunkDispatchType.tsx
@@ -0,0 +1,21 @@
+import { AnyAction } from "redux";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { AllActionsType, AppRootStateType } from "./state";
+
+//типизация для dispacth
+
+export type ActionThunkDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction>;
+//делаем заготовку чтобы передовать уже типизированный useDispatch()
+export const useDispatchWithType = () => useDispatch<ActionThunkDispatchType>();
+
+//универсальная типизация thunk for thunk 
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppRootStateType,
+  unknown,
+  AllActionsType
+>;
+
+//чтобы постоянно не типизировать state. далее используем только типизацию чего достем из state
+export const useSelectorWithType:TypedUseSelectorHook<AppRootStateType> = useSelector
---
+++ src/redusers/app-reducer.test.tsx
@@ -0,0 +1,31 @@
+import { InitialStateType, appReducerStatus, changeTackAppErrorAC, changeTackAppStatusAC } from "./app-reducer";
+
+
+describe("appReducerStatus", () => {
+  let initialState: InitialStateType;
+
+  beforeEach(() => {
+    initialState = {
+      status: "idle",
+      error: null,
+    };
+  });
+
+  test("should change the status", () => {
+    const action = changeTackAppStatusAC("loading");
+
+    const newState = appReducerStatus(initialState, action);
+
+    expect(newState.status).toBe("loading");
+  });
+
+  test("should change the error", () => {
+    const error = "An error occurred";
+    const action = changeTackAppErrorAC(error);
+
+    const newState = appReducerStatus(initialState, action);
+
+    expect(newState.error).toBe(error);
+  });
+
+});
